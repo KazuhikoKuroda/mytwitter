a9be9c37f3297466540e5a14b3bb80a2ea9dec59
KazuhikoKuroda <kuroda2395@gmail.com>
2016-01-21 19:14:14 +0900

    profileルート追加

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -30,6 +30,11 @@ angular
         controller: 'AboutCtrl',
         controllerAs: 'about'
       })
+      .when('/profile', {
+        templateUrl: 'views/profile.html',
+        controller: 'ProfileCtrl',
+        controllerAs: 'profile'
+      })
       .otherwise({
         redirectTo: '/'
       });
---
+++ app/scripts/controllers/profile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mytwitterApp.controller:ProfileCtrl
+ * @description
+ * # ProfileCtrl
+ * Controller of the mytwitterApp
+ */
+angular.module('mytwitterApp')
+  .controller('ProfileCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
