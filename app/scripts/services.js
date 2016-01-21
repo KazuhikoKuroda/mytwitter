a2574656e9ed7996c0b99210f39d6628140b4540
KazuhikoKuroda <kuroda2395@gmail.com>
2016-01-21 17:55:59 +0900

    http://qiita.com/Kurom96/items/59f540619d5cd651dbaf

---
+++ app/scripts/services.js
@@ -0,0 +1,45 @@
+'use strict';
+
+function twitterService($q) {
+	var authResult = false;
+	return {
+		initialize: function() {
+			// OAuth の公開キーを設定する。
+			OAuth.initialize('JNxnfqtFp1IILAedYNeNybsLalI', {
+				cache: true
+			});
+			authResult = OAuth.create('twitter');
+		},
+		isReady: function() {
+			return authResult;
+		},
+		connectTwitter: function() {
+			var deferred = $q.defer();
+			OAuth.popup('twitter').done(function(result) {
+				authResult = result;
+				deferred.resolve();
+			});
+			return deferred.promise;
+		},
+		clearCache: function() {
+			OAuth.clearCache('twitter');
+			authResult = false;
+		},
+		getLatestTweets: function() {
+			var deferred = $q.defer(),
+				url = '/1.1/statuses/home_timeline.json';
+
+			authResult.get(url)
+				.done(function(result) {
+					deferred.resolve(result);
+				})
+				.fail(function(err) {
+					deferred.reject(err);
+				});
+			return deferred.promise;
+		}
+	};
+}
+
+angular.module('mytwitterApp.services', [])
+	.factory('twitterService', twitterService);
